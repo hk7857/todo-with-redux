2f01b3151e44d1927f9298eeefdc07c3d6e7ffa5
Hanran <hanrankim@gmail.com>
2018-04-06 15:36:10 -0700

    Completed getting list data from server

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 import rootReducer from './reducers';
 
 import App from './components/app';
 
 
-const store = createStore(rootReducer, {}, applyMiddleware());
+const store = createStore(rootReducer, {}, applyMiddleware(promise));
 
 ReactDOM.render(
     <Provider store={store}>
