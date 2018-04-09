6468f0924211755080bd3f84fe52abc27d7e9d4a
Hanran <hanrankim@gmail.com>
2018-04-09 11:28:00 -0700

    Single view in progress

---
+++ src/components/item_view.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ItemView extends Component {
+    render(){
+        return(
+            <div>
+                <h3>
+                    View Single Item
+                </h3>
+            </div>
+        )
+    }
+}
+
+export default ItemView;
